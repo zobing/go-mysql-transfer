bc306dc1bacb5cdf44726b846aa1386aff2505e3
zobeen <zobeen@163.com>
2021-06-23 18:06:48 +0800

    【ELK】同步联联商品（tp_lian_product）数据

--- conf/elk/lua/tp_lian_product.lua
+++ conf/elk/lua/tp_lian_product.lua
@@ -20,182 +20,182 @@ result["id"] = id
 
 --[[ 商品信息 开始 --]]
 -- 联联产品索引ID：id
-if row["id"] == nil then
+if row["id"] ~= nil then
     result["lp_id"] = row["id"]
 else
     result["lp_id"] = 0
 end
 
 -- 联联产品ID：lian_id
-if row["lian_id"] == nil then
+if row["lian_id"] ~= nil then
     result["lp_lian_id"] = row["lian_id"]
 else
     result["lp_lian_id"] = 0
 end
 
 -- 联联站点ID | tp_lian_location.lian_id：location_id
-if row["location_id"] == nil then
+if row["location_id"] ~= nil then
     result["lp_location_id"] = row["location_id"]
 else
     result["lp_location_id"] = 0
 end
 
 -- 产品名称：only_name
-if row["only_name"] == nil then
+if row["only_name"] ~= nil then
     result["lp_only_name"] = row["only_name"]
 else
     result["lp_only_name"] = ""
 end
 
 -- 产品标题：title
-if row["title"] == nil then
+if row["title"] ~= nil then
     result["lp_title"] = row["title"]
 else
     result["lp_title"] = ""
 end
 
 -- 产品标题（展示前端）：product_title
-if row["product_title"] == nil then
+if row["product_title"] ~= nil then
     result["lp_product_title"] = row["product_title"]
 else
     result["lp_product_title"] = ""
 end
 
 -- 产品简称(分享文字介绍)：share_text
-if row["share_text"] == nil then
+if row["share_text"] ~= nil then
     result["lp_share_text"] = row["share_text"]
 else
     result["lp_share_text"] = ""
 end
 
 -- 封面图片：face_img
-if row["face_img"] == nil then
+if row["face_img"] ~= nil then
     result["lp_face_img"] = row["face_img"]
 else
     result["lp_face_img"] = ""
 end
 
 -- 商家地址：address
-if row["address"] == nil then
+if row["address"] ~= nil then
     result["lp_address"] = row["address"]
 else
     result["lp_address"] = ""
 end
 
 -- 商家电话：tel
-if row["tel"] == nil then
+if row["tel"] ~= nil then
     result["lp_tel"] = row["tel"]
 else
     result["lp_tel"] = ""
 end
 
 -- 抢购结束时间：end_time
-if row["end_time"] == nil then
+if row["end_time"] ~= nil then
     result["lp_end_time"] = row["end_time"]
 else
     result["lp_end_time"] = 0
 end
 
 -- 抢购开始时间：begin_time
-if row["begin_time"] == nil then
+if row["begin_time"] ~= nil then
     result["lp_begin_time"] = row["begin_time"]
 else
     result["lp_begin_time"] = 0
 end
 
 -- 有效结束时间：valid_end_date
-if row["valid_end_date"] == nil then
+if row["valid_end_date"] ~= nil then
     result["lp_valid_end_date"] = row["valid_end_date"]
 else
     result["lp_valid_end_date"] = 0
 end
 
 -- 有效开始时间：valid_begin_date
-if row["valid_begin_date"] == nil then
+if row["valid_begin_date"] ~= nil then
     result["lp_valid_begin_date"] = row["valid_begin_date"]
 else
     result["lp_valid_begin_date"] = 0
 end
 
 -- 渠道库存：channel_stock
-if row["channel_stock"] == nil then
+if row["channel_stock"] ~= nil then
     result["lp_channel_stock"] = row["channel_stock"]
 else
     result["lp_channel_stock"] = 0
 end
 
 -- 渠道销量：channel_sale_amount
-if row["channel_sale_amount"] == nil then
+if row["channel_sale_amount"] ~= nil then
     result["lp_channel_sale_amount"] = row["channel_sale_amount"]
 else
     result["lp_channel_sale_amount"] = 0
 end
 
 -- 渠道销量：channel_sale_amount
-if row["channel_sale_amount"] == nil then
+if row["channel_sale_amount"] ~= nil then
     result["lp_channel_sale_amount"] = row["channel_sale_amount"]
 else
     result["lp_channel_sale_amount"] = 0
 end
 
 -- 产品分类全路径,使用’-\r\n’分隔子级与父级,父级在前：category_path
-if row["category_path"] == nil then
+if row["category_path"] ~= nil then
     result["lp_category_path"] = row["category_path"]
 else
     result["lp_category_path"] = ""
 end
 
 -- 产品分类 id：product_category_id
-if row["product_category_id"] == nil then
+if row["product_category_id"] ~= nil then
     result["lp_product_category_id"] = row["product_category_id"]
 else
     result["lp_product_category_id"] = 0
 end
 
 -- 外拓状态：0下架；1上架 id：status
-if row["status"] == nil then
+if row["status"] ~= nil then
     result["lp_status"] = row["status"]
 else
     result["lp_status"] = 0
 end
 
 -- 城市编码：city_code
-if row["city_code"] == nil then
+if row["city_code"] ~= nil then
     result["lp_city_code"] = row["city_code"]
 else
     result["lp_city_code"] = ""
 end
 
 -- 当前站点是否可见：is_show
-if row["is_show"] == nil then
+if row["is_show"] ~= nil then
     result["lp_is_show"] = row["is_show"]
 else
     result["lp_is_show"] = 0
 end
 
 -- 套餐状态 0-下架 1-上架 2-售罄：type
-if row["type"] == nil then
+if row["type"] ~= nil then
     result["lp_type"] = row["type"]
 else
     result["lp_type"] = -1
 end
 
 -- 是否需要填写配送地址：booking_show_address
-if row["booking_show_address"] == nil then
+if row["booking_show_address"] ~= nil then
     result["lp_booking_show_address"] = row["booking_show_address"]
 else
     result["lp_booking_show_address"] = 0
 end
 
 -- 预约方式 0-无需预约 1-网址预约 2-电话预约：booking_type
-if row["booking_type"] == nil then
+if row["booking_type"] ~= nil then
     result["lp_booking_type"] = row["booking_type"]
 else
     result["lp_booking_type"] = 0
 end
 
 -- （最低套餐售价的套餐）售价（分）：sale_price
-if row["sale_price"] == nil then
+if row["sale_price"] ~= nil then
     result["lp_sale_price"] = row["sale_price"]
     -- 最低利润
     result["lp_min_profit"] = row["sale_price"] * 0.03
@@ -205,14 +205,14 @@ else
 end
 
 -- （最低套餐售价的套餐）结算价（分）：channel_price
-if row["channel_price"] == nil then
+if row["channel_price"] ~= nil then
     result["lp_channel_price"] = row["channel_price"]
 else
     result["lp_channel_price"] = 0
 end
 
 -- （最低套餐售价的套餐）原价（分）：origin_price
-if row["origin_price"] == nil then
+if row["origin_price"] ~= nil then
     result["lp_origin_price"] = row["origin_price"]
 else
     result["lp_origin_price"] = 0
@@ -227,8 +227,10 @@ end
 
 -- 坐标
 if (row["longitude"] ~= nil) and (row["latitude"] ~= nil) then
-    if (row["longitude"] >= -180) and (row["longitude"] <= 180) and (row["latitude"] >= -90) and (row["latitude"] <= 90) then
-        result["coordinate"] = {row["longitude"], row["latitude"]}
+    local lng = tonumber(row["longitude"])
+    local lat = tonumber(row["latitude"])
+    if (lng >= -180) and (lng <= 180) and (lat >= -90) and (lat <= 90) then
+        result["coordinate"] = {lng, lat}
     else
         result["coordinate"] = {0, 0}
     end
